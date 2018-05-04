5de4e1d89176e337d621c57b9f3dd1819d52bf57
Charbel Wakim <charbel.wakim@keeward.com>
2018-05-04 17:51:05 +0300

    Add neo4j config to db

--- config/database.php
+++ config/database.php
@@ -78,6 +78,15 @@ return [
             'prefix' => '',
         ],
 
+        'neo4j' => [
+            'driver' => 'neo4j',
+            'name' => 'neo4j',
+            'host'   => env('NEO4J_HOST', 'localhost'),
+            'port'   => env('NEO4J_PORT', '7474'),
+            'username' => env('NEO4J_USERNAME', 'neo4j'),
+            'password' => env('NEO4NEO4J_PASSWORD', 'admin'),
+        ],
+
     ],
 
     /*
